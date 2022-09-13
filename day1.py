168f07fe2a503ace6ca26238d16d980d17e38d8b
Oleh Pysmenko <olehpysmenko@blessing.local>
2022-09-13 15:58:49 +0100

    feat: adding html and python files

---
+++ day1.py
@@ -0,0 +1,69 @@
+import math
+from random import randint
+import time
+# userAge = int(input ("Enter your age: "))
+# userLimit = 16
+# passCode = "Cohort11"
+
+# if userAge>userLimit:
+#    print (f"You met minimum age requirements")
+#    userCode= input ("Enter passcode: ")
+#    if passCode==userCode:
+#       print (f"Your code {userCode} is valid. Access granted!")
+#    print("Sorry your code is not valid")
+    
+# print("closing")
+
+# import random
+
+# "Player 1"
+# die1 = random.randint(1, 6)
+# die2 = random.randint(1, 6)
+# print(f"Die 1: {die1}, Die 2: {die2}")
+# dice = die1 + die2  # value from both dice
+# if dice == 12:
+#     dice = 0  # re-assign the value of dice to 0
+#     print("Disqualified")
+# else:
+#     if die1 == die2:
+#         dice = dice * 2
+# player1Score = dice
+# print(f"Player 1 Score : {player1Score}")
+
+
+# "Player 2"
+# die1 = random.randint(1, 6)
+# die2 = random.randint(1, 6)
+
+# print(f"Die 1: {die1}, Die 2: {die2}")
+
+# dice = die1 + die2  # value from both dice
+# if dice == 12:
+#     dice = 0  # re-assign the value of dice to 0
+#     print("Disqualified")
+# else:
+#     if die1 == die2:
+#         dice = dice * 2
+# player2Score = dice
+# print(f"Player 2 Score : {player2Score}")
+
+# # check/compare the values held in player1Score against player2Score
+# if player1Score > player2Score:
+#     print(f"Player 1 rolled {player1Score} and is the winner! ")
+# else:
+#     if player1Score < player2Score:
+#         print(f"Player 2 rolled {player2Score} and is the winner! ")
+#     else:
+#         print(
+#             f"Its a draw! | Player 1 rolled {player1Score} | Player 2 rolled {player2Score}"
+#         )
+
+# radius = float (input ("Enter radius "))
+# area = round((math.pi * radius **2),2)
+
+# print (f"Area is {area}")
+
+# randNumbers = randint(1,100)
+# print (f"the random namber is {randNumbers}")
+
+time.sleep(5)
